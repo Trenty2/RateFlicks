64fc987f68e96e442a93db7f315c746b0fd38610
Trent Sargisson <trent8865@gmail.com>
2020-09-30 08:29:57 +0800

    Add project files.

---
+++ RateFlicks/Data/RateFlicksContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RateFlicks.Models;
+
+namespace RateFlicks.Data
+{
+    public class RateFlicksContext : DbContext
+    {
+        public RateFlicksContext (DbContextOptions<RateFlicksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RateFlicks.Models.Movie> Movie { get; set; }
+    }
+}
